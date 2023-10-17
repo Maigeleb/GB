87db56dcb4c828e7284a230276a3007e8c2ce89d
Mikhail Dorenskiy <michail.1994@mail.ru>
2023-10-17 20:30:11 +0300

    Sixth Seminar

---
+++ SixthSeminar/Task41/Program.cs
@@ -0,0 +1,33 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+Console.WriteLine("Enter numbers:");
+string s = Console.ReadLine();
+int[] elements;
+if (CheckInput(s, out elements))
+{
+    Console.WriteLine($"{s} => {MoreThanZeroCount(elements)}");
+}
+else
+{
+    Console.WriteLine("Incorrect input!");
+}
+
+bool CheckInput(string s, out int[] elements)
+{
+    string[] stringElements = s.Split(',');
+    elements = new int[stringElements.Length];
+
+    for (int i = 0; i < elements.Length; i++)
+        if (!Int32.TryParse(stringElements[i], out elements[i]))
+            return false;
+    return true;
+}
+
+string MoreThanZeroCount(int[] elements)
+{
+    int res = 0;
+
+    for (int i = 0; i < elements.Length; i++)
+        if (elements[i] > 0)
+            res++;
+    return res.ToString();
+}
---
+++ SixthSeminar/Task43/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем
+
+double b1 = GetValue("b1");
+double k1 = GetValue("k1");
+double b2 = GetValue("b2");
+double k2 = GetValue("k2");
+
+CalculateConnectionPoint(b1, k1, b2, k2);
+
+double GetValue(string name)
+{
+    Console.WriteLine($"Enter {name}:");
+    double value;
+    while (!Double.TryParse(Console.ReadLine(), out value))
+    {
+        Console.WriteLine("Incorrect Input!");
+        Console.WriteLine($"Enter {name}:");
+    }
+    return value;
+}
+
+void CalculateConnectionPoint(double b1, double k1, double b2, double k2)
+{
+    if (k1 == k2)
+    {
+        if (b1 == b2)
+            Console.WriteLine("Lines are equal");
+        else
+            Console.WriteLine("Lines are parallel");
+        return;
+    }
+
+    double x = (b1 - b2) / (k2 - k1);
+    double y = k1 * x + b1;
+
+    Console.WriteLine($"Connection Point: [{x},{y}]");
+}
